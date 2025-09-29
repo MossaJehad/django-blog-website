88921a7b185128d98a8a9ef9c91a990f332c123a
MossaJehad <moosa.jehad65@gmail.com>
2025-09-29 17:58:56 +0300

    add login/logout

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin # type: ignore
 from django.urls import path, include # type: ignore
 
 urlpatterns = [
+	path('accounts/', include('django.contrib.auth.urls')),
 	path('admin/', admin.site.urls),
 	path('', include('blog.urls'))
 ]
